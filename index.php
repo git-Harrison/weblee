d1cf6f2ddb82fbb155d20e79d0a5ddf26b5c1c08
DESKTOP-FC21MUR\USER <wognsl305@naver.com>
2021-02-15 17:33:57 +0900

    메일 보내기 폼 + 섹션 2 마크업

--- index.php
+++ index.php
@@ -11,83 +11,141 @@
 	<link rel=" shortcut icon" href="assets/images/favicon.png">
 
 	<!-- 웹 폰트 -->
-	<link href='http://fonts.googleapis.com/earlyaccess/nanumgothic.css' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet">
 
 	<!-- 스타일시트 -->
 	<link rel="stylesheet" href="assets/css/jquery.fullPage.css">
+	<link rel="stylesheet" href="assets/css/common.css">
 	<link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
+	<main id="main">
 
-		<main id="main">
+		<!-- //컨텐츠 2 -->
+		<section class="section" id="section2">
+			<div class="sec_container">
+				<div class="contact_us">
+					<h1>Contact us</h1>
+					<div class="map">
+						<img src="assets/images/map_bg_black.jpg" alt="map">
+					</div>
+					<div class="mail_send_form">
+						<h2>피드백 문의</h2>
+					    <form class="gform" method="POST" data-email="wognsl305@naver.com" action="https://script.google.com/macros/s/AKfycbxZl4zj1MIOahRvkI3GIFg05S9ziv4pmB_aeSA1/exec">
+						    <div class="form-elements">
+								<fieldset class="pure-group">
+									<label for="name"></label>
+									<input id="name" type="text"  value="" required placeholder="Name">
+								</fieldset>
 
-			<!-- 컨텐츠 1 -->
-			<section class="section" id="section1">
-					<div class="sec1_bg_L"></div>
-					<div class="sec1">
-						<div class="cont_L">
-							<h1>WEB | LEE</h1>
-							<div class="lee_img">
-								<img src="assets/images/weblee.jpg" alt="weblee" class="black">
-								<img src="assets/images/weblee_white.jpg" alt="weblee" class="white">
+								<fieldset class="pure-group">
+									<label for="email"></label>
+									<input id="email" type="email" name="email" onblur="this.setAttribute('value', this.value);" value="" required placeholder="E-mail">
+									<span class="validation-text">이메일을 확인해주세요</span>
+								</fieldset>
+
+								<fieldset class="pure-group">
+									<label for="title"></label>
+									<input id="title" name="title" type="text" value="" required placeholder="Title">
+								</fieldset>
+
+								<fieldset class="pure-group">
+									<label for="message"></label>
+									<textarea id="message" name="message" rows="1" required placeholder="Message"></textarea>
+								</fieldset>
+
+								<fieldset class="pure-group honeypot-field">
+									<label for="honeypot"></label>
+									<input id="honeypot" name="honeypot" type="text" name="honeypot" value="" style="display: none;">
+								</fieldset>
+
+								<button class="button-success pure-button button-xlarge">
+								<i class="fa fa-paper-plane"></i>SEND MESSAGE</button>
+						    </div>
+							<div style="display:none" class="thankyou_message">
+								<!-- You can customize the thankyou message by editing the code below -->
+								<h2><em>Thanks</em> for contacting us! We will get back to you soon!
+								</h2>
 							</div>
-							<div class="title">Web Publisher<br>and<br>Fornt-End Developer</div>
-							<div class="sub_title">free worldwide shipping is available for every order.</div>
-						</div>
-						<div class="cont_R">
-							<div class="marker">
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-								<span>WEBLEE</span>
-							</div>
-							<div class="center_title">
-								<h2>SWEATSHIRT</h2>
-								<p>PRODUCT CODE 059608</p>
-							</div>
-							<div id="toggle_theme" class="color_mode">
-								<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none" role="img" class="icon color-palette-icon">
-									<path fill-rule="evenodd" clip-rule="evenodd" d="M22 11C22 17.05 17.05 22 11 22C4.95 22 0 17.05 0 11C0 4.95 4.95 0 11 0C17.05 0 22 4.95 22 11ZM11 19C15.411 19 19 15.411 19 11C19 6.589 15.411 3 11 3V19Z" fill="#3D3D4E"></path>
-								</svg>
+						</form>
+					</div>
+					<div class="our">
+						<div class="our_box">
+							<h4>주소</h4>
+							<div>
+								경기도 수원시
 							</div>
 						</div>
 					</div>
-					<div class="sec1_bg_R"></div>
-			</section>
-			<!-- 컨텐츠 1 -->
+				</div>
+			</div>
+		</section>
+		<!-- //컨텐츠 2 -->
 
-			<!-- //컨텐츠 2 -->
-			<section class="section" id="section2">
-			    ㅇ
-			</section>
-			<!-- //컨텐츠 2 -->
+		<!-- 컨텐츠 1 -->
+		<section class="section" id="section1">
+			<div class="sec_container">
+				<div class="cont_L">
+					<h1>WEB | LEE</h1>
+					<div class="lee_img">
+						<img src="assets/images/weblee.jpg" alt="weblee" class="black">
+						<img src="assets/images/weblee_white.jpg" alt="weblee" class="white">
+					</div>
+					<div class="title">Web Publisher<br>and<br>Fornt-End Developer</div>
+					<div class="sub_title">free worldwide shipping is available for every order.</div>
+				</div>
+				<div class="cont_R">
+					<div class="marker">
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+						<span>WEBLEE</span>
+					</div>
+					<div class="center_title">
+						<h2>SWEATSHIRT</h2>
+						<p>PRODUCT CODE 059608</p>
+					</div>
+					<div id="toggle_theme" class="color_mode">
+						<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none" role="img" class="icon color-palette-icon">
+							<path fill-rule="evenodd" clip-rule="evenodd" d="M22 11C22 17.05 17.05 22 11 22C4.95 22 0 17.05 0 11C0 4.95 4.95 0 11 0C17.05 0 22 4.95 22 11ZM11 19C15.411 19 19 15.411 19 11C19 6.589 15.411 3 11 3V19Z" fill="#3D3D4E"></path>
+						</svg>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- 컨텐츠 1 -->
 
-			<!-- //컨텐츠 3 -->
-			<section class="section" id="section3">
-			    ㅇ
-			</section>
-			<!-- //컨텐츠 3 -->
+		
 
-		</main>
+		<!-- //컨텐츠 3 -->
+		<section class="section" id="section3">
+		    ㅇ
+		</section>
+		<!-- //컨텐츠 3 -->
+
+	</main>
+
+	<script data-cfasync="false" type="text/javascript"
+	src="https://cdn.rawgit.com/dwyl/html-form-send-email-via-google-script-without-server/master/form-submission-handler.js"></script>
+	<script src="assets/js/jquery-1.12.4.js"></script>
+	<script src="assets/js/jquery.fullPage.js"></script>
+	<script src="assets/js/index.js"></script>
 
 </body>
 </html>
-
-<script src="assets/js/jquery-1.12.4.js"></script>
-<script src="assets/js/jquery.fullPage.js"></script>
-<script src="assets/js/index.js"></script>
 
 <? include_once('footer.php'); ?>
