e40070f33c135fdb4cc2edca047f3895a382c4c1
DESKTOP-FC21MUR\USER <wognsl305@naver.com>
2021-03-09 17:03:21 +0900

    파비콘 추가 & 시간표 레이아웃 수정

--- study_setting/css/css.php
+++ study_setting/css/css.php
@@ -8,7 +8,7 @@
 	<title>WebLee</title>
 	
 	<!-- 파비콘 -->
-	<link rel=" shortcut icon" href="../../assets/images/favicon.png">
+	<link rel=" shortcut icon" href="/assets/images/favicon.png">
 
 	<!-- 폰트 -->
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet">
