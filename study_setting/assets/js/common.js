e8bec3e7eb1dc8fb6489a3cdcf62459e3092bf7d
DESKTOP-FC21MUR\USER <wognsl305@naver.com>
2021-03-11 15:58:41 +0900

    body 액션 추가 + pre태그 css 추가

--- study_setting/assets/js/common.js
+++ study_setting/assets/js/common.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$('#nav_icon').click(function(){
 
 		$(this).toggleClass('open');
-		
+		$('body').toggleClass('active');
 		if ($(this).hasClass('open')) {
 			$('.nav').addClass('on');
 			$('.nav_icon_wrap').addClass('on');
