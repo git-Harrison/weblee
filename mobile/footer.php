c20fcf640befd3f2760fee8539f7bafab32f89d9
DESKTOP-FC21MUR\USER <wognsl305@naver.com>
2021-03-29 18:01:55 +0900

    푸터 업데이트

--- mobile/footer.php
+++ mobile/footer.php
@@ -40,6 +40,6 @@
 		</div>
 
 	</footer>
-
+	
 </body>
 </html>
--- mobile/index.php
+++ mobile/index.php
@@ -7,14 +7,6 @@
 	<meta name="keywords" content="웹 퍼블리셔,퍼블리셔,웹 표준,웹 접근성,크로스브라우징,프런트엔드,Front-end,leehun,web developer,publisher,html5,css3,markup,javascript,jquery,php,jsp">
 	<title>WebLee</title>
 
-	<!-- 파비콘 -->
-	<link rel=" shortcut icon" href="../assets/images/favicon.png">
-
-	<!-- 웹 폰트 -->
-	<link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;600;700&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet">
-
 	<!-- 스타일시트 -->
 	<link rel="stylesheet" href="assets/css/index.css">
 
@@ -64,25 +56,7 @@
 			</div>
 		</section>
 
-		<footer>
-			<div>
-				<a href="/">
-					<i class="fa fa-home" aria-hidden="true"></i>
-				</a>
-			</div>
-			<div id="chang_bg_btn">
-				<i class="fa fa-moon-o" aria-hidden="true"></i>
-			</div>
-			<div>
-				<i class="fa fa-envelope-o" aria-hidden="true"></i>
-			</div>
-			<div onclick="change_lang()">
-				<i class="fa fa-language" aria-hidden="true"></i>
-			</div>
-		</footer>
-
 	</div>
-
 
 	<!-- 스크립트 -->
 	<script src="assets/js/jquery-3.4.1.js"></script>
